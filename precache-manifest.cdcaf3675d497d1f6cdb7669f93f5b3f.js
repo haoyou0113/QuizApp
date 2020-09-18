945c6b1357410c2e0beea4244a9def5494031116
YouHao <haoyou0807@163.com>
2020-09-18 18:13:44 +1000

    Updates

---
+++ precache-manifest.cdcaf3675d497d1f6cdb7669f93f5b3f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35d560d6c1d32260339cbb69b38a1b3",
+    "url": "/QuizApp/index.html"
+  },
+  {
+    "revision": "2b101dcd43bce57db9db",
+    "url": "/QuizApp/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "ef327d146237208e2442",
+    "url": "/QuizApp/static/css/main.8153c7e8.chunk.css"
+  },
+  {
+    "revision": "2b101dcd43bce57db9db",
+    "url": "/QuizApp/static/js/2.4c9a3f2d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/QuizApp/static/js/2.4c9a3f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef327d146237208e2442",
+    "url": "/QuizApp/static/js/main.01b7eb1b.chunk.js"
+  },
+  {
+    "revision": "da24ce78299903fdbc7e",
+    "url": "/QuizApp/static/js/runtime-main.76e2bcf6.js"
+  }
+]);
